179bb534afc70ebef07eab504d67e3a22c4fbcb6
DevineJasper <jasper.vermeesch@student.howest.be>
2020-02-10 15:25:41 +0100

    font bug fixed

--- server/app/routes/drinks.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-  const controller = require("../controllers/drinks.controller.js");
-  app.post("/api/drinks", controller.create);
-  app.get("/api/drinks", controller.findAll);
-  app.get("/api/drinks/:drinkId", controller.findOne);
-  app.put("/api/drinks/:drinkId", controller.update);
-  app.delete("/api/drinks/:drinkId", controller.delete);
-};

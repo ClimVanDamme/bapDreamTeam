719dd4980ea6fdbd0f57dfe337e93b7fb96f5edb
DevineJasper <jasper.vermeesch@student.howest.be>
2020-02-11 17:46:43 +0100

    uitleg pagina css responsive

--- client/src/components/Uitleg.jsx
+++ client/src/components/Uitleg.jsx
@@ -1,64 +1,60 @@
-import React from "react";
-import { inject, observer } from "mobx-react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { inject, observer } from 'mobx-react';
+import { Link } from 'react-router-dom';
 
-import Button from "./Button";
-import Title from "./Title";
-import * as uuid from "uuid/v4";
-import TitleMiddle from "../components/TitleMiddle";
+import Button from './Button';
+import Title from './Title';
+import * as uuid from 'uuid/v4';
+import TitleMiddle from '../components/TitleMiddle';
 
-import styleUitleg from "./Uitleg.module.css";
-import styleUI from "../styles/uiControls.module.css";
+import styleUitleg from './Uitleg.module.css';
+import styleUI from '../styles/uiControls.module.css';
 
 const Uitleg = ({ path, titel, uitleg, buttons }) => {
-  console.log("in uitleg");
-  console.log(titel);
-  console.log(buttons);
-  console.log("in de uitleg comp");
-  return (
-    <div className={styleUitleg.containerGrid}>
-      <section className={styleUitleg.content}>
-        <TitleMiddle keyValue={uuid()} text="Supporterslied." />
-        {/* <img
-				className={styleUitleg.challengeImg}
-				src={`./assets/img/${titel}@2x.png`}
-				alt={`${titel} foto`}
-			/> */}
-        <div className={styleUI.tussenTitelCont}>
-          <h2 className={styleUI.visuallyHidden}>Hoe werkt het?</h2>
-        </div>
-        <p>{uitleg}</p>
-        <div>
-          <ul className={styleUitleg.selectionContainer}>
-            {buttons.map((button, index) => {
-              return (
-                <li
-                  className={`styleUitleg.btnCont styleUitleg.button${index}`}
-                  key={index}
-                >
-                  <img
-                    className={styleUitleg.btnImg}
-                    src={`./assets/img/${button.img}.png`}
-                    alt=""
-                    width="72"
-                    height="72"
-                  />
-                  <div className={styleUitleg.btnCenter}>
-                    <Button
-                      keyValue={uuid()}
-                      label={button.label}
-                      color={"primary"}
-                      link={`${path}/${button.path}`}
-                    />
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </section>
-    </div>
-  );
+	console.log('in uitleg');
+	console.log(titel);
+	console.log(buttons);
+	console.log('in de uitleg comp');
+	return (
+		<div className={styleUitleg.containerGrid}>
+			<section className={styleUitleg.content}>
+				<div className={styleUitleg.textPart}>
+					<TitleMiddle keyValue={uuid()} text='Supporterslied.' />
+					<div className={styleUI.tussenTitelCont}>
+						<h2 className={styleUI.visuallyHidden}>Hoe werkt het?</h2>
+					</div>
+					<p>{uitleg}</p>
+				</div>
+				<div className={styleUitleg.buttonPart}>
+					<ul className={styleUitleg.selectionContainer}>
+						{buttons.map((button, index) => {
+							return (
+								<li className={`${styleUitleg.btnCont}${index}`} key={index}>
+									<div className={styleUitleg.imgCenter}>
+										<img
+											className={styleUitleg.btnImg}
+											src={`./assets/img/${button.img}.png`}
+											alt=''
+											width='72'
+											height='72'
+										/>
+									</div>
+									<div className={styleUitleg.btnCenter}>
+										<Button
+											keyValue={uuid()}
+											label={button.label}
+											color={'primary'}
+											link={`${path}/${button.path}`}
+										/>
+									</div>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</section>
+		</div>
+	);
 };
 
 // Sporza.propTypes = {

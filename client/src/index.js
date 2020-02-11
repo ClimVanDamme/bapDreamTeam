33b0614e043bb05cc6a1c6cafbfab1bf829299fa
DevineJasper <jasper.vermeesch@student.howest.be>
2020-02-11 09:59:02 +0100

    server side rendering test

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,32 @@
-import { Provider } from "mobx-react";
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./containers/App";
-import * as serviceWorker from "./serviceWorker";
-import store from "./store";
-import "./styles/index.css";
+import { Provider } from 'mobx-react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './containers/App';
+import * as serviceWorker from './serviceWorker';
+import store from './store';
+import './styles/index.css';
 
-ReactDOM.render(
-  <Provider {...store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+const rootElement = document.getElementById(`root`);
 
-  document.getElementById(`root`)
-);
+if (rootElement.hasChildNodes()) {
+	ReactDOM.hydrate(
+		<Provider {...store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>,
+		rootElement
+	);
+} else {
+	ReactDOM.render(
+		<Provider {...store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>,
+		rootElement
+	);
+}
+
 serviceWorker.register();
--- server/index.js
+++ server/index.js
@@ -1,11 +1,45 @@
-const express = require('express');
-const path = require('path');
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// app.get('/*', function(req, res) {
+// 	res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
+
+// app.listen(process.env.PORT || 4000);
+
+import { Helmet } from 'react-helmet';
 const app = express();
+const path = require('path');
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+const server = () => {
+	app.get('/*', (req, res) => {
+		const app = renderToString(<App />, document.getElementById('root'));
+		const helmet = Helmet.renderStatic();
 
-app.listen(process.env.PORT || 4000);
+		const html = `
+<!doctype html>
+<html ${helmet.htmlAttributes.toString()}>
+    <head>
+        ${helmet.title.toString()}
+        ${helmet.meta.toString()}
+        ${helmet.link.toString()}
+    </head>
+    <body ${helmet.bodyAttributes.toString()}>
+        <div id="app">
+            ${app}
+        </div>
+    </body>
+</html>
+`;
+
+		res.send(html);
+	});
+
+	app.listen(process.env.PORT || 4000);
+};
+export default server;

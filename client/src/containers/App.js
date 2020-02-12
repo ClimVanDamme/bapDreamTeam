050b461ab2406f5fcdcb9cbdec198d82aee2c1a6
DevineJasper <jasper.vermeesch@student.howest.be>
2020-02-12 03:38:31 +0100

    homepage styling finished

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,282 +1,292 @@
-import React, { Component } from "react";
-import { Route, Switch, withRouter } from "react-router-dom";
-import styles from "../styles/layout.module.css";
+import React, { Component } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import styles from '../styles/layout.module.css';
 
-import { Helmet } from "react-helmet";
+import { Helmet } from 'react-helmet';
 
-import { ROUTES } from "../constants";
+import { ROUTES } from '../constants';
 
 //import containers
-import Home from "./Home";
-import ResultaatContainer from "./ResultaatContainer";
+import Home from './Home';
+import ResultaatContainer from './ResultaatContainer';
 //
-import Uitleg from "../components/Uitleg";
-import RouteContainer from "./RouteContainer";
+import Uitleg from '../components/Uitleg';
+import RouteContainer from './RouteContainer';
 //
 // import Belofte from './Belofte';
-import BelofteContainer from "./BelofteContainer";
+import BelofteContainer from './BelofteContainer';
 //
-import Supporterslied from "./Supporterslied";
-import OverzichtSupporterslied from "./OverzichtSupporterslied";
-import AudioInput from "./AudioInput";
+import Supporterslied from './Supporterslied';
+import OverzichtSupporterslied from './OverzichtSupporterslied';
+import AudioInput from './AudioInput';
 //
 //import TapWedstrijd from "./aaTapWedstrijd";
-import TapInput from "./TapInput";
+import TapInput from './TapInput';
 //
-import SmoothieMaker from "./SmoothieMaker";
-import SmoothieMixer from "./SmoothieMixer";
+import SmoothieMaker from './SmoothieMaker';
+import SmoothieMixer from './SmoothieMixer';
 //
-import PostkaartStudent from "./PostkaartStudent";
-import PostkaartVakantieganger from "./PostkaartVakantieganger";
-import PostkaartWerkmens from "./PostkaartWerkmens";
+import PostkaartStudent from './PostkaartStudent';
+import PostkaartVakantieganger from './PostkaartVakantieganger';
+import PostkaartWerkmens from './PostkaartWerkmens';
+//
+import { CSSTransitionGroup } from 'react-transition-group';
 
 class App extends Component {
-  render() {
-    return (
-      <main className={styles.layout}>
-        <Switch>
-          {/* HOME */}
-          <Route key={1} exact path={ROUTES.home}>
-            <Home />
-            <Helmet>
-              <title>De Spelen zijn van ons</title>
-              <meta
-                name="description"
-                content="Steun de Belgische atleten op de Olympische Spelen 2020 in Tokio. Aangeboden door Sporza."
-              />
-              <meta
-                name="tags"
-                content="campagne, sporza, olympische spelen, tokio, 2020"
-              />
-            </Helmet>
-          </Route>
-          {/* BELOFTE */}
-          <RouteContainer
-            key={2}
-            path={ROUTES.belofte}
-            routes={[
-              {
-                component: "uitleg",
-                path: "",
-                titel: "Olympische Belofte",
-                description: "Maak een belofte gedurende de Olympische Spelen.",
-                uitleg:
-                  "Kies welk soort belofte je wilt aangaan met iemand en vul vervolgens de namen ins ;)",
-                buttons: [
-                  { label: "Afkick", path: "afkick", img: "afkickImg" },
-                  { label: "Voornemen", path: "voornemen", img: "voornemenImg" }
-                ]
-              },
-              {
-                component: <BelofteContainer soort={"afkick"} />,
-                path: "afkick",
-                titel: "Overzicht fragmenten",
-                description: "100 meter tappen",
-                uitleg: "",
-                buttons: ""
-              },
-              {
-                component: <BelofteContainer soort={"voornemen"} />,
-                path: "voornemen",
-                titel: "Overzicht fragmenten",
-                description: "100 meter tappen",
-                uitleg: "",
-                buttons: ""
-              },
-              {
-                component: <ResultaatContainer soort={"afkick"} />,
-                path: "resultaat",
-                titel: "Jouw belofte",
-                description: "Jouw belofte",
-                uitleg: "",
-                buttons: "",
-                backgroundImg: ""
-              }
-            ]}
-          />
-          {/* SUPPORTERSLIED */}
-          <RouteContainer
-            key={3}
-            path={ROUTES.supporterslied}
-            routes={[
-              {
-                component: "uitleg",
-                path: "",
-                titel: "Supporterslied.",
-                description:
-                  "Maak je eigen supporterslied om de Belgische atleten op de Olympische Spelen aan te moedigen!",
-                uitleg:
-                  "Laat horen hoe jij iemand aanmoedigt en maak een supporterslied, alleen of samen met vrienden.",
-                buttons: [{ label: "Start >", path: "overzicht" }]
-              },
-              {
-                component: <OverzichtSupporterslied />,
-                path: "overzicht",
-                titel: "Overzicht fragmenten",
-                description: "100 meter tappen",
-                uitleg: "",
-                buttons: ""
-              },
-              {
-                component: <AudioInput />,
-                path: "opnemen",
-                titel: "Opnemen nieuwe laag",
-                description: "Opnemen nieuwe laag supporterslied",
-                uitleg: "",
-                buttons: ""
-              },
-              {
-                component: <ResultaatContainer soort={"supporterslied"} />,
-                path: "resultaat",
-                titel: "Jouw supporterslied",
-                description: "Jouw supporterslied",
-                uitleg: "",
-                buttons: "",
-                backgroundImg: ""
-              }
-            ]}
-          />
-          {/* tapwedstrijd */}
-          <RouteContainer
-            key={4}
-            path={ROUTES.tapWedstrijd}
-            routes={[
-              {
-                component: "uitleg",
-                path: "",
-                titel: "Tapwedstrijd",
-                description:
-                  "Laat je inspireren voor een frisse, gezonde en Belgische smoothie!",
-                uitleg:
-                  "Kies op welke afstand jij jezelf wilt bewijzen en tap zo snel mogelijk naar de finish!",
-                buttons: [
-                  { label: "100 meter >", path: "100", img: "select_100m@2x" },
-                  { label: "200 meter >", path: "200", img: "select_200m@2x" },
-                  { label: "400 meter >", path: "400", img: "select_400m@2x" }
-                ]
-              },
-              {
-                component: <TapInput max={100} />,
-                path: "100",
-                titel: "100m Tapwedstrijd",
-                description: "100 meter tappen",
-                uitleg: "",
-                buttons: ""
-              },
-              {
-                component: <TapInput max={200} />,
-                path: "200",
-                titel: "200m Tapwedstrijd",
-                description: "200 meter tappen",
-                uitleg: "",
-                buttons: ""
-              },
-              {
-                component: <TapInput max={400} />,
-                path: "400",
-                titel: "400m Tapwedstrijd",
-                description: "400 meter tappen",
-                uitleg: "",
-                buttons: ""
-              },
-              {
-                component: <ResultaatContainer soort={"tapwedstrijd"} />,
-                path: "resultaat",
-                titel: "Jouw score",
-                description: "Jouw score",
-                uitleg: "",
-                buttons: "",
-                backgroundImg: ""
-              }
-            ]}
-          />
-          {/* smoothieMaker */}
-          <RouteContainer
-            key={5}
-            path={ROUTES.smoothieMaker}
-            routes={[
-              {
-                component: "uitleg",
-                path: "",
-                titel: "Smoothie Maker",
-                description:
-                  "Laat je inspireren voor een frisse, gezonde en Belgische smoothie!",
-                uitleg:
-                  "Druk op de mix knop en bekijk verrassende combinaties van fruit om in jouw smoothie te steken.",
-                buttons: [{ label: "beginnen", path: "mixer" }]
-              },
-              {
-                component: <SmoothieMixer />,
-                path: "mixer",
-                titel: "Smoothie Mixer",
-                description:
-                  "Laat je inspireren voor een frisse, gezonde en Belgische smoothie!",
-                uitleg: "",
-                buttons: ""
-              }
-            ]}
-          />
-          {/* POSTKAART */}
-          <RouteContainer
-            key={6}
-            path={ROUTES.postkaartje}
-            routes={[
-              {
-                component: "uitleg",
-                path: "",
-                titel: "Postkaartje",
-                description: "Stuur iemand een Olympisch postkaartje",
-                uitleg:
-                  "Kies voor welke persoon je een kaartje wilt schrijven, kies daarna uit een kaartje en voeg vervolgens een persoonlijk tekstje op. Het resultaat kan je versturen ;)",
-                buttons: [
-                  { label: "student", path: "student" },
-                  { label: "vakantieganger", path: "vakantieganger" },
-                  { label: "werkmens", path: "werkmens" }
-                ]
-              },
-              {
-                component: <PostkaartStudent />,
-                path: "student",
-                titel: "Smoothie Mixer",
-                description:
-                  "Laat je inspireren voor een frisse, gezonde en Belgische smoothie!",
-                uitleg: "",
-                buttons: ""
-              },
-              {
-                component: <PostkaartVakantieganger />,
-                path: "vakantieganger",
-                titel: "Smoothie Mixer",
-                description:
-                  "Laat je inspireren voor een frisse, gezonde en Belgische smoothie!",
-                uitleg: "",
-                buttons: ""
-              },
-              {
-                component: <PostkaartWerkmens />,
-                path: "werkmens",
-                titel: "Smoothie Mixer",
-                description:
-                  "Laat je inspireren voor een frisse, gezonde en Belgische smoothie!",
-                uitleg: "",
-                buttons: ""
-              },
-              {
-                component: <ResultaatContainer soort={"postkaartstudent"} />,
-                path: "resultaat",
-                titel: "Jouw persoonlijke postkaart",
-                description: "Jouw persoonlijke postkaart",
-                uitleg: "",
-                buttons: "",
-                backgroundImg: ""
-              }
-            ]}
-          />
-          ;
-        </Switch>
-      </main>
-    );
-  }
+	render() {
+		return (
+			<main>
+				<Switch>
+					{/* HOME */}
+					<Route key={1} exact path={ROUTES.home}>
+						<CSSTransitionGroup
+							transitionName='example'
+							transitionAppear={true}
+							transitionAppearTimeout={500}
+							transitionEnter={false}
+							transitionLeave={true}
+						>
+							<Home />
+						</CSSTransitionGroup>
+						<Helmet>
+							<title>De Spelen zijn van ons</title>
+							<meta
+								name='description'
+								content='Steun de Belgische atleten op de Olympische Spelen 2020 in Tokio. Aangeboden door Sporza.'
+							/>
+							<meta
+								name='tags'
+								content='campagne, sporza, olympische spelen, tokio, 2020'
+							/>
+						</Helmet>
+					</Route>
+					{/* BELOFTE */}
+					<RouteContainer
+						key={2}
+						path={ROUTES.belofte}
+						routes={[
+							{
+								component: 'uitleg',
+								path: '',
+								titel: 'Olympische Belofte',
+								description: 'Maak een belofte gedurende de Olympische Spelen.',
+								uitleg:
+									'Kies welk soort belofte je wilt aangaan met iemand en vul vervolgens de namen ins ;)',
+								buttons: [
+									{ label: 'Afkick', path: 'afkick', img: 'afkickImg' },
+									{ label: 'Voornemen', path: 'voornemen', img: 'voornemenImg' }
+								]
+							},
+							{
+								component: <BelofteContainer soort={'afkick'} />,
+								path: 'afkick',
+								titel: 'Overzicht fragmenten',
+								description: '100 meter tappen',
+								uitleg: '',
+								buttons: ''
+							},
+							{
+								component: <BelofteContainer soort={'voornemen'} />,
+								path: 'voornemen',
+								titel: 'Overzicht fragmenten',
+								description: '100 meter tappen',
+								uitleg: '',
+								buttons: ''
+							},
+							{
+								component: <ResultaatContainer soort={'afkick'} />,
+								path: 'resultaat',
+								titel: 'Jouw belofte',
+								description: 'Jouw belofte',
+								uitleg: '',
+								buttons: '',
+								backgroundImg: ''
+							}
+						]}
+					/>
+					{/* SUPPORTERSLIED */}
+					<RouteContainer
+						key={3}
+						path={ROUTES.supporterslied}
+						routes={[
+							{
+								component: 'uitleg',
+								path: '',
+								titel: 'Supporterslied.',
+								description:
+									'Maak je eigen supporterslied om de Belgische atleten op de Olympische Spelen aan te moedigen!',
+								uitleg:
+									'Laat horen hoe jij iemand aanmoedigt en maak een supporterslied, alleen of samen met vrienden.',
+								buttons: [{ label: 'Start >', path: 'overzicht' }]
+							},
+							{
+								component: <OverzichtSupporterslied />,
+								path: 'overzicht',
+								titel: 'Overzicht fragmenten',
+								description: '100 meter tappen',
+								uitleg: '',
+								buttons: ''
+							},
+							{
+								component: <AudioInput />,
+								path: 'opnemen',
+								titel: 'Opnemen nieuwe laag',
+								description: 'Opnemen nieuwe laag supporterslied',
+								uitleg: '',
+								buttons: ''
+							},
+							{
+								component: <ResultaatContainer soort={'supporterslied'} />,
+								path: 'resultaat',
+								titel: 'Jouw supporterslied',
+								description: 'Jouw supporterslied',
+								uitleg: '',
+								buttons: '',
+								backgroundImg: ''
+							}
+						]}
+					/>
+					{/* tapwedstrijd */}
+					<RouteContainer
+						key={4}
+						path={ROUTES.tapWedstrijd}
+						routes={[
+							{
+								component: 'uitleg',
+								path: '',
+								titel: 'Tapwedstrijd',
+								description:
+									'Laat je inspireren voor een frisse, gezonde en Belgische smoothie!',
+								uitleg:
+									'Kies op welke afstand jij jezelf wilt bewijzen en tap zo snel mogelijk naar de finish!',
+								buttons: [
+									{ label: '100 meter >', path: '100', img: 'select_100m@2x' },
+									{ label: '200 meter >', path: '200', img: 'select_200m@2x' },
+									{ label: '400 meter >', path: '400', img: 'select_400m@2x' }
+								]
+							},
+							{
+								component: <TapInput max={100} />,
+								path: '100',
+								titel: '100m Tapwedstrijd',
+								description: '100 meter tappen',
+								uitleg: '',
+								buttons: ''
+							},
+							{
+								component: <TapInput max={200} />,
+								path: '200',
+								titel: '200m Tapwedstrijd',
+								description: '200 meter tappen',
+								uitleg: '',
+								buttons: ''
+							},
+							{
+								component: <TapInput max={400} />,
+								path: '400',
+								titel: '400m Tapwedstrijd',
+								description: '400 meter tappen',
+								uitleg: '',
+								buttons: ''
+							},
+							{
+								component: <ResultaatContainer soort={'tapwedstrijd'} />,
+								path: 'resultaat',
+								titel: 'Jouw score',
+								description: 'Jouw score',
+								uitleg: '',
+								buttons: '',
+								backgroundImg: ''
+							}
+						]}
+					/>
+					{/* smoothieMaker */}
+					<RouteContainer
+						key={5}
+						path={ROUTES.smoothieMaker}
+						routes={[
+							{
+								component: 'uitleg',
+								path: '',
+								titel: 'Smoothie Maker',
+								description:
+									'Laat je inspireren voor een frisse, gezonde en Belgische smoothie!',
+								uitleg:
+									'Druk op de mix knop en bekijk verrassende combinaties van fruit om in jouw smoothie te steken.',
+								buttons: [{ label: 'beginnen', path: 'mixer' }]
+							},
+							{
+								component: <SmoothieMixer />,
+								path: 'mixer',
+								titel: 'Smoothie Mixer',
+								description:
+									'Laat je inspireren voor een frisse, gezonde en Belgische smoothie!',
+								uitleg: '',
+								buttons: ''
+							}
+						]}
+					/>
+					{/* POSTKAART */}
+					<RouteContainer
+						key={6}
+						path={ROUTES.postkaartje}
+						routes={[
+							{
+								component: 'uitleg',
+								path: '',
+								titel: 'Postkaartje',
+								description: 'Stuur iemand een Olympisch postkaartje',
+								uitleg:
+									'Kies voor welke persoon je een kaartje wilt schrijven, kies daarna uit een kaartje en voeg vervolgens een persoonlijk tekstje op. Het resultaat kan je versturen ;)',
+								buttons: [
+									{ label: 'student', path: 'student' },
+									{ label: 'vakantieganger', path: 'vakantieganger' },
+									{ label: 'werkmens', path: 'werkmens' }
+								]
+							},
+							{
+								component: <PostkaartStudent />,
+								path: 'student',
+								titel: 'Smoothie Mixer',
+								description:
+									'Laat je inspireren voor een frisse, gezonde en Belgische smoothie!',
+								uitleg: '',
+								buttons: ''
+							},
+							{
+								component: <PostkaartVakantieganger />,
+								path: 'vakantieganger',
+								titel: 'Smoothie Mixer',
+								description:
+									'Laat je inspireren voor een frisse, gezonde en Belgische smoothie!',
+								uitleg: '',
+								buttons: ''
+							},
+							{
+								component: <PostkaartWerkmens />,
+								path: 'werkmens',
+								titel: 'Smoothie Mixer',
+								description:
+									'Laat je inspireren voor een frisse, gezonde en Belgische smoothie!',
+								uitleg: '',
+								buttons: ''
+							},
+							{
+								component: <ResultaatContainer soort={'postkaartstudent'} />,
+								path: 'resultaat',
+								titel: 'Jouw persoonlijke postkaart',
+								description: 'Jouw persoonlijke postkaart',
+								uitleg: '',
+								buttons: '',
+								backgroundImg: ''
+							}
+						]}
+					/>
+					;
+				</Switch>
+			</main>
+		);
+	}
 }
 
 export default withRouter(App);
--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, Component } from 'react';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../constants';
 
@@ -12,60 +12,156 @@ import Title from '../components/Title';
 
 import * as uuid from 'uuid/v4';
 
-const Home = () => {
-	return (
-		<>
-			<header className={stylesHome.headerGrid}>
-				<div className={stylesHome.headerTitle}>
-					<Title
-						keyValue={uuid()}
-						text={[
-							'De Spelen ',
-							<br />,
-							'zijn van ',
-							<span className={stylesHome.headerO}>o</span>,
-							<span className={stylesHome.headerN}>n</span>,
-							<span className={stylesHome.headerS}>s</span>
-						]}
-					/>
-				</div>
-				<div className={stylesHome.headerText}>
-					<p>
-						De Belgische atleten geven het beste van zichzelf op de Spelen in
-						Tokio. Da de Olympische uitdagingen aan en daag zoveel mogelijk
-						mensen uit. En toon zo dat we achter Team Belgium staan!
-					</p>
-					<div className={stylesHome.headerBtn}>
-						<Button
+import { CSSTransitionGroup } from 'react-transition-group';
+
+const scrollToRef = ref => window.scrollTo(0, ref.current.offsetTop);
+
+class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.latest = React.createRef();
+	}
+
+	scrollToLatest = () => {
+		scrollToRef(this.latest);
+	};
+
+	render = () => {
+		return (
+			<>
+				<header className={stylesHome.headerGrid}>
+					<div className={stylesHome.headerTitle}>
+						<Title
 							keyValue={uuid()}
-							label={'ga naar nieuwste opdracht'}
-							color={'secondary'}
-							link={'#latest'}
+							text={[
+								'De Spelen ',
+								<br />,
+								'zijn van ',
+								<span className={stylesHome.headerO}>o</span>,
+								<span className={stylesHome.headerN}>n</span>,
+								<span className={stylesHome.headerS}>s</span>
+							]}
 						/>
 					</div>
-				</div>
-				<img
-					className={stylesHome.headerImg}
-					src='../assets/img/header_image@2x.png'
-					alt='sporza_atleten_home'
-				/>
-				<div className={stylesHome.headerScroll}>
-					<p style={{ textAlign: 'center', textTransform: 'uppercase' }}>
-						scroll verder
-					</p>
-					<p className={stylesHome.scrollAnimation}>﹀</p>
-				</div>
-			</header>
-			<section className={stylesHome.sectionChallenges}>
-				<div className={stylesHome.tijdslijn}>
-					<HomeChallenge challenge={'belofte'} />
-					<HomeChallenge challenge={'supporterslied'} />
-					<HomeChallenge challenge={'postkaartje'} />
-					<HomeChallenge challenge={'tapwedstrijd'} />
-				</div>
-			</section>
-		</>
-	);
-};
+					<div className={stylesHome.headerText}>
+						<p>
+							De Belgische atleten geven het beste van zichzelf op de Spelen in
+							Tokio. Da de Olympische uitdagingen aan en daag zoveel mogelijk
+							mensen uit. En toon zo dat we achter Team Belgium staan!
+						</p>
+						<div className={stylesHome.headerBtn}>
+							<div onClick={this.scrollToLatest}>
+								<Button
+									keyValue={uuid()}
+									label={'ga naar nieuwste opdracht'}
+									color={'secondary'}
+									link={''}
+								/>
+							</div>
+						</div>
+					</div>
+					<img
+						className={stylesHome.headerImg}
+						src='../assets/img/header_image@2x.png'
+						alt='sporza_atleten_home'
+					/>
+					<div className={stylesHome.headerScroll}>
+						<p
+							className={stylesHome.scrollText}
+							style={{ textAlign: 'center', textTransform: 'uppercase' }}
+						>
+							scroll verder
+						</p>
+						<p className={stylesHome.scrollAnimation}>﹀</p>
+					</div>
+				</header>
+				<section className={stylesHome.challengesWrapper}>
+					<div className={stylesHome.challengesContent}>
+						<CSSTransitionGroup
+							transitionName='example'
+							transitionAppear={true}
+							transitionAppearTimeout={500}
+							transitionEnter={false}
+							transitionLeave={true}
+						>
+							<HomeChallenge
+								challenge={'belofte'}
+								direction={'links'}
+								unlockDate={'23 juli'}
+							/>
+						</CSSTransitionGroup>
+						<CSSTransitionGroup
+							transitionName='example'
+							transitionAppear={true}
+							transitionAppearTimeout={500}
+							transitionEnter={false}
+							transitionLeave={true}
+						>
+							<HomeChallenge
+								challenge={'supporterslied'}
+								direction={'links'}
+								unlockDate={'26 juli'}
+							/>
+						</CSSTransitionGroup>
+						<CSSTransitionGroup
+							transitionName='example'
+							transitionAppear={true}
+							transitionAppearTimeout={500}
+							transitionEnter={false}
+							transitionLeave={true}
+						>
+							<HomeChallenge
+								challenge={'postkaartje'}
+								direction={'rechts'}
+								unlockDate={'29 juli'}
+							/>
+						</CSSTransitionGroup>
+						<CSSTransitionGroup
+							transitionName='example'
+							transitionAppear={true}
+							transitionAppearTimeout={500}
+							transitionEnter={false}
+							transitionLeave={true}
+						>
+							<div ref={this.latest}>
+								<HomeChallenge
+									challenge={'tapwedstrijd'}
+									direction={'rechts'}
+									unlockDate={'1 augustus'}
+								/>
+							</div>
+						</CSSTransitionGroup>
+						<CSSTransitionGroup
+							transitionName='example'
+							transitionAppear={true}
+							transitionAppearTimeout={500}
+							transitionEnter={false}
+							transitionLeave={true}
+						>
+							<HomeChallenge
+								challenge={'quiz'}
+								direction={'links'}
+								unlockDate={'4 augustus'}
+							/>
+						</CSSTransitionGroup>
+						<CSSTransitionGroup
+							transitionName='example'
+							transitionAppear={true}
+							transitionAppearTimeout={500}
+							transitionEnter={false}
+							transitionLeave={true}
+						>
+							<HomeChallenge
+								challenge={'smoothie'}
+								direction={'links'}
+								unlockDate={'7 augustus'}
+							/>
+						</CSSTransitionGroup>
+					</div>
+				</section>
+			</>
+		);
+	};
+}
 
 export default Home;
